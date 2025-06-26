7651c68656e51015941b804a67181d55485da481
Braith May <braith.may@wisetechglobal.com>
2025-06-26 14:30:23 +1000

    :sparkles: Add landing page :sparkles:

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto my-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Hey there,
+        </h1>
+        <p class="py-6">
+          Save your travel memories with Travel Logger, the app that helps you document your journeys and share them with friends and family.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
