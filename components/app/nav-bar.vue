2c7cb96df2974afbb3ab655b2a16c71bad7de3b3
Braith May <braith.may@wisetechglobal.com>
2025-06-27 17:31:21 +1000

    :sparkles: Add theme toggler :sparkles:

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -6,6 +6,7 @@
       </Nuxtlink>
     </div>
     <div class="navbar-end">
+      <AppThemeToggle />
       <a class="btn btn-accent">
         Sign In With Github
         <Icon name="tabler:brand-github" size="24" />
