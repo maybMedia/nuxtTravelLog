48772c153e128135578ce327d023992cc948dcce
Braith May <braith.may@wisetechglobal.com>
2025-07-10 14:05:39 +1000

    ☕ Implement GitHub Sign In ☕

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import AuthButton from "~/components/auth-button.vue";
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto my-4">
     <div class="hero-content text-center min-h-96">
@@ -8,10 +12,7 @@
         <p class="py-6">
           Save your travel memories with Travel Logger, the app that helps you document your journeys and share them with friends and family.
         </p>
-        <button class="btn btn-accent">
-          Sign In With Github
-          <Icon name="tabler:brand-github" size="24" />
-        </button>
+        <AuthButton />
       </div>
     </div>
   </div>
